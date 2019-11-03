4fa575ba5600557a30fc202be011c9ab57eb5d8a
Ranjith Cheguri <theeverindian@gmail.com>
2019-11-02 17:03:37 -0700

    Front End Theme Setup

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+var ENV_VAR = require('./config/config');
+var pool = require('./config/mysql');
+var mysql=require('mysql')
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
+var morgan = require('morgan');
+
+app.use(express.static('uploads'));
+
+
+// Log requests to console
+app.use(morgan('dev'));
+
+app.use(cors({ origin: ENV_VAR.CORS_ORIGIN, credentials: true }));
+app.use(session({
+    secret: 'opportunity_hack',
+    resave: true, // Forces the session to be saved back to the session store, even if the session was never modified during the request
+    saveUninitialized: false, // Force to save uninitialized session to db. A session is uninitialized when it is new but not modified.
+    duration: 60 * 60 * 1000,    // Overall duration of Session : 30 minutes : 1800 seconds
+    activeDuration: 5 * 60 * 1000
+}));
+
+//Allow Access Control
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', ENV_VAR.CORS_ORIGIN);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+
+app.listen(ENV_VAR.PORT);
+console.log("Server running on port " + ENV_VAR.PORT);
